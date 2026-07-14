904ff4a388e6990c06c00315eba7d1c219a7b856
obeyaniki <sho.0614game@gmail.com>
2026-07-15 08:47:07 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// Google Cloudで取得したクライアントID
+const CLIENT_ID =
+"318247976957-an3qcrmiqou86tlobbkfq6trhmtl6h4r.apps.googleusercontent.com";
+
+// ★ここに自分のAPIキーを貼ってください
+const API_KEY =
+"AIzaSyD9u62NE-qXUzPMzsyQH9LjwNQ1AKSvEn4";
+
+// Gmail API
+const DISCOVERY_DOC =
+"https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest";
+
+// 読み取り専用
+const SCOPES =
+"https://www.googleapis.com/auth/gmail.readonly";
